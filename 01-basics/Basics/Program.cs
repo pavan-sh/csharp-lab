60ca3ec1e8a8afa24b37a0c0d68599cc4a1bd7bd
Pavan Shinde <pavann97@gmail.com>
2026-08-10 08:30:14 +0530

    feat: added simple salary calculator - Part 1

--- 01-basics/Basics/Program.cs
+++ 01-basics/Basics/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+Simple Salary Calculator
+*/
+
+Console.WriteLine("\nEnter your salary:");
+
+string userSalaryInput = Console.ReadLine();
+
+int salary = int.Parse(userSalaryInput);
+
+//Console.WriteLine("\nEntered Salary is- \t" + salary+ "\n");
+
+Console.WriteLine("\nChoose Tax Regime (Enter a or b) \n a) Old Tax Regime b) New Tax Regime: ");
+
+string userTaxRegime = Console.ReadLine();
+
+char taxRegime = char.Parse(userTaxRegime);
+
+double tax = 0;
+
+if(taxRegime=='a'){
+    if(salary <= 250000){
+        tax = 0;
+    }
+    else if(salary <= 500000){
+        // apply tax on amount above 250000
+        tax = (salary - 250000) * 0.05;
+    }
+    else if(salary <= 1000000){
+        tax = 12500 + (salary - 500000) * 0.20;
+    }
+    else {
+        tax = 112500 + (salary - 1000000) * 0.30;
+    }
+    double salaryAfterTax = salary - tax;
+
+    Console.WriteLine("\nWith old tax regime, salary is: "+salaryAfterTax+"\n");
+} else {
+    Console.WriteLine("\nWith new tax regime, salary is: \n");
+}
